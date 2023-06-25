74071c1d76701a976ea84e0b9c1c73a557edde82
Chanwoo Kim <ckim463@gatech.edu>
2023-06-25 15:10:06 -0400

    integrated google maps api

--- foodie/src/components/Restaurant.js
+++ foodie/src/components/Restaurant.js
@@ -1,42 +1,32 @@
 import React from "react";
-import { GoogleMap, Marker, useLoadScript } from "react-google-maps/api";
+import { GoogleMap, Marker, useLoadScript } from "@react-google-maps/api";
 import { useMemo } from "react";
 import styled from "styled-components";
 
+const containerStyle = {
+    width: "400px",
+    height: "400px",
+};
 
 const Restaurant = () => {
+    const REACT_APP_GOOGLE_MAPS_API_KEY =
+        "AIzaSyDC2SFtvrCtHCJkCdWKiXoLTn8CI7F8pIg";
     const { isLoaded } = useLoadScript({
-        googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+        googleMapsApiKey: REACT_APP_GOOGLE_MAPS_API_KEY,
     });
-    const center = useMemo(() => ({ lat: 33.753746, long: -84.386330 }), []);
-
+    const center = useMemo(() => ({ lat: 33.753746, lng: -84.38633 }), []);
 
     return (
         <div className="Map">
             {!isLoaded ? (
                 <h1>Loading...</h1>
             ) : (
-                <GoogleMap
-                    mapContainerClassName="map-container"
-                    center={center}
-                    zoom={10}>
-
-                    <Marker position={{ lat: 33.753746, long: -84.386330 }} />
+                <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10}>
+                    <Marker position={{ lat: 33.753746, lng: -84.38633 }} />
                 </GoogleMap>
-
             )}
         </div>
     );
 };
-const RestaurantStyle = styled.div`
-.Map {
-    height: 100vh;
-    width: 100vh;
-}
 
-.map-container {
-    height: 100%;
-    width: 100%;
-}
-`;
 export default Restaurant;
